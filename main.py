2fc107fc90197e401d253307de0a66739881d33e
fred <winterautomotivellc@gmail.com>
2025-08-31 11:46:39 -0400

    Refactoring of last commit while also trying OpenGL for smoother animations.

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 from PyQt6.QtWidgets import QApplication
 from openauto.ui.home import MainWindow
-import os
-from PyQt6 import QtCore
+import os, platform
+from PyQt6 import QtCore, QtGui
 
+### FORCE XWAYLAND IF USING LINUX ###
+if platform.system() == "Linux" and os.environ.get("XDG_SESSION_TYPE", "").lower() == "wayland":
+    os.environ["QT_QPA_PLATFORM"] = "xcb"
 
+try:
+    QApplication.setAttribute(QtCore.Qt.ApplicationAttribute.AA_UseDesktopOpenGL, True)
+    fmt = QtGui.QSurfaceFormat()
+    fmt.setSwapBehavior(QtGui.QSurfaceFormat.SwapBehavior.DoubleBuffer)
+    fmt.setSwapInterval(1)
+    QtGui.QSurfaceFormat.setDefaultFormat(fmt)
+except Exception as e:
+    print("OpenGL setup failed, falling back:", e)
 
 if __name__ == "__main__":
-    if os.environ.get("XDG_SESSION_TYPE") == "wayland":
-        os.environ["QT_QPA_PLATFORM"] = "xcb"
     app = QApplication([])
     window = MainWindow()
     window.show()
-    QtCore.QTimer.singleShot(0, window.showMaximized)
-    print("Device Pixel Ratio:", app.primaryScreen().devicePixelRatio())
+    QtCore.QTimer.singleShot(1, window.showMaximized)
     app.exec()
 
